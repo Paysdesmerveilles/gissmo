d1d055b4bb445cad77ceec1f1fbbfb5b1c19e698
Fabien Engels <fabien.engels@gmail.com>
2011-12-01 14:52:11 +0100

    Release v0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = "bdmateriel",
-    version = "0.2",
+    version = "0.3",
     author = "Martin Dutil",
     author_email = "mdutil@unistra.fr",
     description = "BD suivi materiel",
