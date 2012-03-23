a055e86b37089efa5298bc0b62ecfafd3226e40c
Martin Dutil <mdutil@unistra.fr>
2012-03-23 14:30:04 +0100

    change on static file tree and use {{ STATIC_URL }} in template

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name = "bdmateriel",
-    version = "0.91",
+    version = "0.92",
     author = "Martin Dutil",
     author_email = "mdutil@unistra.fr",
     description = "BD suivi materiel",
     url = "https://teatime.u-strasbg.fr/prj/projects/bdmatos/",
     packages = ['bdmateriel','bdmateriel.templatetags',],
     package_dir = {'bdmateriel': 'src/bdmateriel'},
-    package_data = {'bdmateriel': ['templates/*.html','templates/admin/*.html','templates/admin/bdmateriel/*.html','templates/admin/bdmateriel/stationsite/*.html','static/*.png']},
+    package_data = {'bdmateriel': ['templates/*.html','templates/admin/*.html','templates/admin/bdmateriel/*.html','templates/admin/bdmateriel/stationsite/*.html','static/img/*.png']},
 )
