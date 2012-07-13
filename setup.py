0e2ffed178466cc06d54814767359c8e0f6db2f4
Martin Dutil <mdutil@unistra.fr>
2012-07-13 10:43:27 +0200

    setup.py modification for revision 1.0

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name = "bdmateriel",
-    version = "0.97",
+    version = "1.0",
     author = "Martin Dutil",
     author_email = "mdutil@unistra.fr",
     description = "BD suivi materiel",
     url = "https://teatime.u-strasbg.fr/prj/projects/bdmatos/",
     packages = ['bdmateriel','bdmateriel.templatetags',],
     package_dir = {'bdmateriel': 'src/bdmateriel'},
-    package_data = {'bdmateriel': ['templates/*.html','templates/admin/*.html','templates/admin/bdmateriel/*.html','templates/admin/bdmateriel/stationsite/*.html','static/img/*.png']},
+    package_data = {'bdmateriel': ['templates/*.html','templates/admin/*.html','templates/admin/bdmateriel/*.html','templates/admin/bdmateriel/equipment/*.html','templates/admin/bdmateriel/stationsite/*.html','static/img/*.png','static/js/*.js']},
 )
