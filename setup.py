ba0a19dcda3f8b851a3b8e2eeed4fda5bc9cbd3d
Martin Dutil <mdutil@unistra.fr>
2013-03-12 16:43:39 +0100

    Add of the station.xml template in the setup

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name = "bdmateriel",
-    version = "1.13r",
+    version = "1.14r",
     author = "Martin Dutil",
     author_email = "mdutil@unistra.fr",
     description = "BD suivi materiel",
     url = "https://teatime.u-strasbg.fr/prj/projects/bdmatos/",
     packages = ['bdmateriel','bdmateriel.templatetags',],
     package_dir = {'bdmateriel': 'src/bdmateriel'},
-    package_data = {'bdmateriel': ['templates/*.html','templates/admin/*.html','templates/admin/bdmateriel/*.html','templates/admin/bdmateriel/channel/*.html','templates/admin/bdmateriel/equipment/*.html','templates/admin/bdmateriel/network/*.html','templates/admin/bdmateriel/stationsite/*.html','static/img/*.png','static/js/*.js']},
+    package_data = {'bdmateriel': ['templates/*.html','templates/*.xml','templates/admin/*.html','templates/admin/bdmateriel/*.html','templates/admin/bdmateriel/channel/*.html','templates/admin/bdmateriel/equipment/*.html','templates/admin/bdmateriel/network/*.html','templates/admin/bdmateriel/stationsite/*.html','static/img/*.png','static/js/*.js']},
 )
