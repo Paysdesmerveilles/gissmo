d9acdf1f6440bc23bd7dfcfd23adc3b92499061f
Martin Dutil <mdutil@unistra.fr>
2013-08-21 16:39:40 +0200

    Version adjustement

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = "bdmateriel",
-    version = "1.22r",
+    version = "1.23r",
     author = "Martin Dutil",
     author_email = "mdutil@unistra.fr",
     description = "BD suivi materiel",
