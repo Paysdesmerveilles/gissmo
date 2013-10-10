a35e78167b4bdb26ff21ef2ad90d6c262226bc6b
Fabien Engels <fabien.engels@gmail.com>
2013-10-10 11:05:36 +0200

    Start to fix "DeprecationWarning: django.conf.urls.defaults"

--- gissmo/urls/common.py
+++ gissmo/urls/common.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import include, patterns, url
+from django.conf.urls import include, patterns, url
 from django.conf.urls.static import static
 
 from django.contrib import admin
