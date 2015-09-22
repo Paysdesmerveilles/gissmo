eba21c80c2329e38aa9d4a1f70acfe0c9530de52
Olivier DOSSMANN <olivier+git@dossmann.net>
2015-09-22 11:04:45 +0200

    [IMP] Functional tests: Now tests checks that some used URLs are OK

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -39,6 +39,8 @@ class SimpleTest(unittest.TestCase):
 #            except KeyError as e:
 #                self.port = None
         self.url = 'http://%s:%s/' % (self.ip, self.port)
+        self.adminurl = self.url + 'gissmo/'
+        self.appurl = self.adminurl + 'gissmo/'
 
     def tearDown(self):
         """
@@ -58,16 +60,16 @@ class SimpleTest(unittest.TestCase):
         # He receive a 404 error: Page not found. He remembers that we should 
         #+ type /gissmo after the URL
         self.assertIn('Page not found', self.browser.title, 
-            'Problem loading %s' % url)
-        url = self.url + 'gissmo/'
+            'Problem loading %s' % self.browser.current_url)
+        url = self.adminurl
         self.browser.get(url)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Gissmo', header_text, 
-            'Problem loading %s' % url)
+            'Problem loading %s' % self.browser.current_url)
 
         # He puts his username and password to connect the admin interface
         self.assertIn('Log in', self.browser.title, 
-            'Problem loading %s: Not login page' % url)
+            'Problem loading %s: Not login page' % self.browser.current_url)
         inputlogin = self.browser.find_element_by_id('id_username')
         inputpassword = self.browser.find_element_by_id('id_password')
         inputlogin.send_keys(DEFAULT_ADMIN_LOGIN)
@@ -78,14 +80,45 @@ class SimpleTest(unittest.TestCase):
         self.assertIn('Site administration', self.browser.title, 
             'Login problem on %s: no administration interface found.')
 
-        # He gets "Sites" list
-        self.fail('Finish the test!')
-
-        # He gets "Modèle des équipements" list
-
-        # He gets "Equipements" list
-
-        # He gets "Interventions" list
+        # As Martin don't know how the website works he will browse it
+        # So it checks some links
+        links_to_check = [
+            'stationsite/',
+            'equipmodel/',
+            'equipment/',
+            'intervention/',
+            'actor/',
+            'network/',
+            'project/',
+            'equipmodeldoctype/',
+            'equipdoctype/',
+            'projectuser/',
+            'stationdoctype/',
+            'builttype/',
+            'equiptype/',
+            'calibrationunit/',
+            'datatype/',
+            'parameterequip/',
+            'parametervalue/',
+            'channel/',
+            'channelcode/'
+        ]
+        for code in links_to_check:
+            url = self.appurl + code
+            self.browser.get(url)
+            self.assertNotIn('Page not found', self.browser.title, 
+                "Not found URL: %s" % self.browser.current_url)
+            self.assertIn('Select', self.browser.title,
+                "No 'select' on title. %s" % self.browser.current_url)
+            self.assertIn('to change', self.browser.title,
+                "No 'to change' on title. %s" % self.browser.current_url)
+        
+        # As Martin is curious, he changes the URL using imagination.
+        # But the application said "Page not found".
+        url = self.appurl + 'sdflkj'
+        self.browser.get(url)
+        self.assertIn('Page not found', self.browser.title,
+                "This page SHOULD NOT be found! %s" % self.browser.current_url)
 
 if __name__ == "__main__":
     unittest.main()
