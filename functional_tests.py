8e2cc36c2bf05c08e0aa88717bac92df0c0eaf54
Olivier DOSSMANN <olivier+git@dossmann.net>
2015-09-22 11:35:57 +0200

    [ADD] Functional tests:
      * Log in function for tests
      * Test "Station Map" page

--- functional_tests.py
+++ functional_tests.py
@@ -48,6 +48,21 @@ class SimpleTest(unittest.TestCase):
         """
         self.browser.quit()
 
+    def gissmo_login(self):
+        """
+        Log in to Gissmo application
+        """
+        url = self.adminurl
+        self.browser.get(url)
+        inputlogin = self.browser.find_element_by_id('id_username')
+        inputpassword = self.browser.find_element_by_id('id_password')
+        inputlogin.send_keys(DEFAULT_ADMIN_LOGIN)
+        inputpassword.send_keys(DEFAULT_ADMIN_PASSWORD)
+        inputpassword.send_keys(Keys.ENTER)
+        # Stop tests if login failed
+        self.assertIn('Site administration', self.browser.title, 
+            'Login problem on %s: no administration interface found.')
+
     def test_can_get_main_pages(self):
         """
         Check that each page don't send a 404 error message
@@ -118,7 +133,35 @@ class SimpleTest(unittest.TestCase):
         url = self.appurl + 'sdflkj'
         self.browser.get(url)
         self.assertIn('Page not found', self.browser.title,
-                "This page SHOULD NOT be found! %s" % self.browser.current_url)
+            "This page SHOULD NOT be found! %s" % self.browser.current_url)
+
+    def test_station_map_loading(self):
+        """
+        Check that Station Map is loaded.
+        """
+        # Yvette wants to check Station Map.
+        # She log in to Gissmo.
+        self.gissmo_login()
+
+        # Then she goes to Site List to check the Station Map link
+        url = self.appurl + 'stationsite/'
+        self.browser.get(url)
+        links = self.browser.find_elements_by_tag_name('a')
+        self.assertNotEqual(links, [], 
+            "No links found on this page: %s" % self.browser.current_url)
+        tofind_url = self.adminurl + 'site_maps/'
+        self.assertTrue(
+            any(link.get_attribute('href') == tofind_url for link in links),
+            "No link found to get Station Map here: %s. Searching for: %s" % 
+                (self.browser.current_url, tofind_url)
+        )
+
+        # Station Map link is here. So she finally click on it to see result
+        url = self.adminurl + 'site_maps/'
+        self.browser.get(url)
+        self.assertIn('RESIF', self.browser.title,
+            "Title should contains RESIF on %s. Current: %s." % 
+            (self.browser.current_url, self.browser.title))
 
 if __name__ == "__main__":
     unittest.main()
