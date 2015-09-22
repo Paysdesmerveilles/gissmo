a4cc37b3a831fc4d16737aeade34f264ed5e390a
Olivier DOSSMANN <olivier+git@dossmann.net>
2015-09-22 10:30:13 +0200

    [ADD] Functional tests: Site administration log in

--- functional_tests.py
+++ functional_tests.py
@@ -6,6 +6,8 @@ from docker import Client
 
 DOCKER_CONTAINER_NAME = 'gissmo'
 DOCKER_CONTAINER_DEFAULT_PORT = '8000'
+DEFAULT_ADMIN_LOGIN = 'olivier'
+DEFAULT_ADMIN_PASSWORD = 'olivier'
 
 class SimpleTest(unittest.TestCase):
     """
@@ -50,19 +52,34 @@ class SimpleTest(unittest.TestCase):
         """
         # Martin Dupont start using the site. He goes to check the website 
         #+ homepage.
-        self.browser.get(self.url)
+        url = self.url
+        self.browser.get(url)
 
         # He receive a 404 error: Page not found. He remembers that we should 
         #+ type /gissmo after the URL
-        self.assertIn('Page not found', self.browser.title, 'Problem loading %s' % self.url)
-        self.browser.get(self.url + 'gissmo/')
+        self.assertIn('Page not found', self.browser.title, 
+            'Problem loading %s' % url)
+        url = self.url + 'gissmo/'
+        self.browser.get(url)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('Gissmo', header_text)
+        self.assertIn('Gissmo', header_text, 
+            'Problem loading %s' % url)
 
         # He puts his username and password to connect the admin interface
-        self.fail('Finish the test!')
+        self.assertIn('Log in', self.browser.title, 
+            'Problem loading %s: Not login page' % url)
+        inputlogin = self.browser.find_element_by_id('id_username')
+        inputpassword = self.browser.find_element_by_id('id_password')
+        inputlogin.send_keys(DEFAULT_ADMIN_LOGIN)
+        inputpassword.send_keys(DEFAULT_ADMIN_PASSWORD)
+
+        # When he hits enter, the page updates, and now administration appears
+        inputpassword.send_keys(Keys.ENTER)
+        self.assertIn('Site administration', self.browser.title, 
+            'Login problem on %s: no administration interface found.')
 
         # He gets "Sites" list
+        self.fail('Finish the test!')
 
         # He gets "Modèle des équipements" list
 
