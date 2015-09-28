830a8915d1a4aa89963f21e8e4fa5fae6c2a0e71
Olivier DOSSMANN <olivier+git@dossmann.net>
2015-09-28 11:28:31 +0200

    [UP] Make gissmo.settings to be PEP8 compatible

--- gissmo/settings/dev.py
+++ gissmo/settings/dev.py
@@ -1,4 +1,5 @@
-from gissmo.settings.common import *
+from gissmo.settings.common import *  # NOQA
+from os import getenv
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -12,13 +13,13 @@ ROOT_URLCONF = 'gissmo.urls.dev'
 
 INSTALLED_APPS += (
     'django_extensions',
-    'functional_tests', # for function test purposes
+    'functional_tests',  # for function test purposes
 )
 
 # Update DB settings for docker-compose use
 DATABASES['default'].update(
     {
-        'HOST': os.getenv('DB_PORT_5432_TCP_ADDR', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT_5432_TCP_PORT', '5433'),
+        'HOST': getenv('DB_PORT_5432_TCP_ADDR', '127.0.0.1'),
+        'PORT': getenv('DB_PORT_5432_TCP_PORT', '5433'),
     }
 )
