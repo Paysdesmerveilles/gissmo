6c44ea9343b564d225670ae169604d800d9bac4b
Olivier DOSSMANN <olivier+git@dossmann.net>
2015-11-20 13:26:44 +0100

    [FIX] API: Change webservice API script to fetch data regarding pagination feature

--- scripts/webservice_use_example.py
+++ scripts/webservice_use_example.py
@@ -13,15 +13,26 @@ auth_url = 'http://%s/api-auth/login/' % (server)
 print("### Display all sites: ###")
 url = service_url + 'site/?format=json'
 
-# Fetch URL
-request = requests.get(url, auth=(username, password))
-if request.status_code != 200:
-    request.raise_for_status()
-# Use JSON result
-js = request.json()
-# Parse sites
-for site in js:
-    name = site.get('name', '')
-    code = site.get('code', '')
-    _type = site.get('type', 'Unknown')
-    print('%s: %s. (%s)' % (code, name, _type))
+
+# Functions
+def fetch_data(url):
+    # Fetch URL
+    request = requests.get(url, auth=(username, password))
+    if request.status_code != 200:
+        request.raise_for_status()
+    # Use JSON result
+    js = request.json()
+    next_page = js.get('next', None)
+    return js, next_page
+
+# As API activates pagination, we need to fetch more than 1 URL
+js, next_page = fetch_data(url)
+while next_page:
+    result = js.get('results', [])
+    # Parse sites
+    for site in result:
+        name = site.get('name', '')
+        code = site.get('code', '')
+        _type = site.get('type', 'Unknown')
+        print('%s: %s. (%s)' % (code, name, _type))
+    js, next_page = fetch_data(next_page)
