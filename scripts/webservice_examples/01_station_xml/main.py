25c8b6b3a1020580f7bed856e754df826ab9fef7
Olivier DOSSMANN <olivier+git@dossmann.net>
2015-11-20 17:15:25 +0100

    [WIP] API: Start working on a station_xml constructor using WebService API

---
+++ scripts/webservice_examples/01_station_xml/main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import requests
+import sys
+
+# Configuration
+server = 'localhost:8002'
+username = 'olivier'
+password = 'olivier'
+apiurl = 'http://%s/api/' % server
+api_sites = '%ssites/' % apiurl
+api_channels = '%schannels/' % apiurl
+
+
+class Station(object):
+    def __init__(self, data_dict):
+        fields = [
+            'id',
+            'code',
+            'latitude',
+            'longitude',
+            'elevation',
+            'name',
+            'region',
+            'county',
+            'country',
+            'town',
+        ]
+        for field in fields:
+            setattr(self, field, data_dict.get(field, None))
+        self.channels = []
+
+    def get_channels(self):
+        res = ''
+        for channel in self.channels:
+            res += '  %s\n' % str(channel)
+        return res
+
+    def __str__(self):
+        result = """<Station code="%s">
+  %s</Station>"""
+        return result % (self.code, self.get_channels())
+
+
+class Channel(object):
+    def __init__(self, data_dict):
+        fields = [
+            'id',
+            'code',
+            'location_code',
+            'start_date',
+        ]
+        for field in fields:
+            setattr(self, field, data_dict.get(field, None))
+
+    def __str__(self):
+        result = """<Channel code="%s" locationCode="%s" startDate="%s">
+  </Channel>"""
+        return result % (self.code, self.location_code, self.start_date)
+
+
+def get_result(request):
+    res = None
+    js = request.json()
+    if js:
+        res = js.get('results', None)
+    return res
+
+
+def get(url):
+    data = []
+    request = requests.get(url, auth=(username, password))
+    if request:
+        data = get_result(request)
+    return data
+
+# Search Charmoille site
+url = api_sites + '?name=CHARMOILLE'
+stations = get(url)
+for station in stations:
+    # Fetch its ID and some info
+    s = Station(station)
+    # Search linked channels
+    url = api_channels + '?station=%s' % s.code
+    channels = get(url)
+    for channel in channels:
+        # Fetch code, location code
+        c = Channel(channel)
+        s.channels.append(c)
+    print(s)
+# Get all channels info
+
+sys.exit(0)
