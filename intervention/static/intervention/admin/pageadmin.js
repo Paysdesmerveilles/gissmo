d64be58366a6d4e7e93f314d2a710e09d5c66b61
Olivier DOSSMANN <olivier+git@dossmann.net>
2015-12-15 11:20:13 +0100

    [IMP] Simplify javascript that fetch allowed transitions for a given Site.

--- intervention/static/intervention/admin/pageadmin.js
+++ intervention/static/intervention/admin/pageadmin.js
@@ -2,20 +2,21 @@
  * Get allowed transition regarding choosen site.
 */
 function get_transitions(select, url){
-  var site_id = select.options.selectedIndex;
-  django.jQuery.ajax({
+  var site_id = select.options[select.selectedIndex].value;
+  var $ = django.jQuery;
+  $.ajax({
     url: url,
     data: { site: site_id },
     dataType: 'json',
     success: function(result, status, xhr) {
       var transitions = result.allowed_transitions;
-      var select = django.jQuery('td.field-transition select')
-      var options = django.jQuery('td.field-transition select option')
+      var select = $('td.field-transition select')
+      var options = $('td.field-transition select option')
       // first remove all available options
       options.remove()
       // then display compatible ones
-      django.jQuery.each(transitions, function(i){
-        select.append(django.jQuery("<option></option>")
+      $.each(transitions, function(i){
+        select.append($("<option></option>")
                .attr("value", i)
                .text(transitions[i]));
       });
