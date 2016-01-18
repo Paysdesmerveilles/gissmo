77c58ee6821d9122671e806ce969b425e8c5b0fd
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-01-18 16:12:43 +0100

    [FIX] Tests: problem with new homepage redirection that doesn't pass tests

--- gissmo/tests.py
+++ gissmo/tests.py
@@ -8,12 +8,12 @@ DEFAULT_ADMIN_PASSWORD = 'admin'
 
 class HomePageTest(TestCase):
 
-    def test_root_url_returns_404(self):
+    def test_root_url_returns_302(self):
         response = self.client.get('/')
         self.assertEqual(
             response.status_code,
-            404,
-            "Root page shouldn't be found.")
+            302,
+            "Root page should be redirected!")
 
 
 class AdminPageTest(TestCase):
