b9ffdeecd389cb778c485fb76249b2af150dc9ac
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-02-17 11:11:51 +0100

    [UP] Return to trunk (dev) version

--- gissmo/urls/dev.py
+++ gissmo/urls/dev.py
@@ -8,7 +8,7 @@ from django.conf.urls import (
     patterns,
     include)
 
-admin.site.site_header = 'Gissmo 1.5-rc1'
+admin.site.site_header = 'Gissmo 1.5-trunk'
 
 if dev.DEBUG:
     import debug_toolbar
