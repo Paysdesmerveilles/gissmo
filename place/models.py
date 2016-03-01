7a85e2c68ce35fba5791c652435bc18eee21df32
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-03-01 15:52:24 +0100

    [ADD] New objects: Site, Built, BuiltType and Station.
      * split StationSite into Station and Site
      * Create new objects like Built and BuiltType

--- place/models.py
+++ place/models.py
@@ -123,9 +123,160 @@ def get_used_code(sender, instance, **kwargs):
     instance.code = pstate.USED
 
 
-class Site(models.Model):
-    name = models.CharField(max_length=60, unique=True)
+class CommonPosition(models.Model):
+    # units
+    DEGREES = 0
+    METERS = 1
+    POSITION_UNIT_CHOICES = (
+        (DEGREES, "Degrees"),
+        (METERS, "Meters"),
+    )
+
+    # fields
+    latitude = models.DecimalField(
+        null=True,
+        blank=True,
+        max_digits=9,
+        decimal_places=6,
+        verbose_name="Latitude (°)")
+    longitude= models.DecimalField(
+        null=True,
+        blank=True,
+        verbose_name="Longitude (°)",
+        max_digits=9,
+        decimal_places=6)
+    elevation= models.DecimalField(
+        null=True,
+        blank=True,
+        verbose_name="Elevation (m)",
+        max_digits=5,
+        decimal_places=1)
+    latitude_datum = models.CharField(
+        max_length=15,
+        null=True,
+        blank=True,
+        default="WGS84",
+        verbose_name="Latitude datum")
+    longitude_datum = models.CharField(
+        max_length=15,
+        null=True,
+        blank=True,
+        default="WGS84",
+        verbose_name="Longitude datum")
+    latitude_unit = models.IntegerField(
+        choices=POSITION_UNIT_CHOICES,
+        default=DEGREES,
+        null=True,
+        blank=True,
+        verbose_name="Latitude unit")
+    longitude_unit = models.IntegerField(
+        choices=POSITION_UNIT_CHOICES,
+        default=DEGREES,
+        null=True,
+        blank=True,
+        verbose_name="Longitude unit")
+    elevation_unit = models.IntegerField(
+        choices=POSITION_UNIT_CHOICES,
+        default=METERS,
+        null=True,
+        blank=True,
+        verbose_name="Elevation unit")
+
+    class Meta:
+        abstract = True
+
+
+class Site(CommonPosition):
+    """
+    A place on which you achieve some tasks. As:
+      * testing some devices
+      * sending broken devices (customer service place)
+      * installing a station (make a link from a station to a site)
+      * have a lot of builts
+      * searching for a new area to install a station
+    """
+    # Site types
+    OBSERVATORY = 0
+    CUSTOMER_SERVICE = 1
+    UNDEFINED = 2
+    OTHER = 3
+    TEST = 4
+    THEORITICAL = 5
+    SITE_TYPE_CHOICES = (
+        (OBSERVATORY, 'Observatory'),
+        (CUSTOMER_SERVICE, 'Customer service place'),
+        (UNDEFINED, 'Undefined'),
+        (OTHER, 'Other'),
+        (TEST, 'Testing site'),
+        (THEORITICAL, 'Theoritical site'),
+    )
+
+    # fields
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        verbose_name="Referent site")
+    name = models.CharField(max_length=50, unique=True)
+    _type = models.IntegerField(
+        choices=SITE_TYPE_CHOICES,
+        default=THEORITICAL,  # This is to make user works more about type :)
+        verbose_name="Type")
+    description = models.TextField(
+        null=True,
+        blank=True)
+    # TODO: check state design if revelant with GISSMO 2.0 AND MOVE STATE
+    # to STATION object in Network as Station is those that change its state
     state = models.ForeignKey('State', null=True, related_name='current_state')
+    creation_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name="Creation date")
+
+    # address
+    address_street = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name="Street")
+    address_zipcode = models.CharField(
+        max_length=15,
+        null=True,
+        blank=True,
+        verbose_name="Zip code")
+    address_city = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name="City")
+    address_region = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name="Region")
+    address_county = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name="District")
+    address_country = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True,
+        verbose_name="Country")
+
+    # miscellaneous
+    geology = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True,
+        verbose_name="Geological formation")
+
+    # folks
+    operator = models.ForeignKey('user.Player')
+    contact = models.TextField(
+        null=True,
+        blank=True)
 
     def __str__(self):
         return self.name
@@ -140,3 +291,24 @@ def create_available_state(sender, instance, created, **kwargs):
         a = StateAvailable.objects.create(site=instance)
         instance.state = a
         instance.save()
+
+
+class BuiltType(models.Models):
+    name = models.CharField(max_length=40)
+
+    class Meta:
+        verbose_name = "Built type"
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Built(CommonPosition):
+    name = models.CharField(
+        max_length=40,
+        default="Unknown")
+    _type = models.ForeignKey("place.BuiltType", verbose_name="Type")
+    site = models.ForeignKey("place.Site")
+
+    def __str__(self):
+        return '%s' % self.name
