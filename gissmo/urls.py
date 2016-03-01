84f6f9ecb4b3db7e4e235e1c21ab21723a62cdf9
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-03-01 10:52:42 +0100

    [IMP] Rename 'mytest' project to 'gissmo'

--- gissmo/urls.py
+++ gissmo/urls.py
@@ -7,7 +7,7 @@ admin.site.site_header = 'The best sandbox area you ever seen!'
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'mytest.views.home', name='home'),
+    # url(r'^$', 'gissmo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
