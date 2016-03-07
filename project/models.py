d4ebf3800504cd2c3e098be88928d7b7eecbe8e8
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-03-07 10:33:23 +0100

    [FIX] Project: wrong field denomination

--- project/models.py
+++ project/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=50)
     manager = models.ForeignKey('user.Player')
-    sites = models.ManyToMany('place.Site', blank=True)
-    users = models.ManyToMany('user.GissmoUser', blank=True)
+    sites = models.ManyToManyField('place.Site', blank=True)
+    users = models.ManyToManyField('user.GissmoUser', blank=True)
