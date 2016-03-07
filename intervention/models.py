cca02053379f7cdd40f7af4389e8b8ffc6683f6c
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-03-07 11:59:39 +0100

    [IMP] Change Intervention to only be linked to Players

--- intervention/models.py
+++ intervention/models.py
@@ -1,44 +1,8 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
-
-from polymorphic.models import PolymorphicModel
-
-from place import transitions as ptransition
 
 
 class Intervention(models.Model):
     date = models.DateTimeField(db_index=True)
     note = models.TextField(null=True, blank=True)
-    players = models.ManyToManyField('user.Player', through='InterventionPlayer')
     site = models.ForeignKey('place.Site')
-
-
-class InterventionPlayer(models.Model):
-    intervention = models.ForeignKey('Intervention', on_delete=models.CASCADE)
-    player = models.ForeignKey('user.Player', on_delete=models.CASCADE)
-
-
-class ActionSite(models.Model):
-    # TODO: Link to a built
-    transition = models.IntegerField(
-        choices=ptransition.TRANSITION_CHOICES)
-    note = models.TextField(null=True, blank=True)
-    intervention = models.ForeignKey('Intervention')
-
-    class Meta:
-        verbose_name = _('intervention on site')
-        verbose_name_plural = _('Site interventions')
-
-
-class ActionEquipment(models.Model):
-    # TODO: link to an equipment
-    # TODO: link to equipment transitions
-#    transition = models.IntegerField(
-#        choices=etransition.TRANSITION_CHOICES)
-    # TODO: link to a built
-    note = models.TextField(null=True, blank=True)
-    intervention = models.ForeignKey('Intervention')
-
-    class Meta:
-        verbose_name = _('intervention on equipment')
-        verbose_name_plural = _('Equipment interventions')
+    players = models.ManyToManyField('user.Player')
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from user.models import (
+    GissmoUser,
+    Player)
+
+# Players
+admin.site.unregister(User)
+admin.site.register(GissmoUser)
+admin.site.register(Player)
