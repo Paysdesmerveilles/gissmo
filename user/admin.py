132a4e2119913ff66f0e5f03705824e79dd8d5cc
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-03-21 15:25:40 +0100

    [FIX] Organisms - change users display to add more than 1 user easily

--- user/admin.py
+++ user/admin.py
@@ -7,6 +7,7 @@ class OrganismAdmin(admin.ModelAdmin):
     list_display = ['name', '_type', 'parent']
     list_filter = ['_type']
     search_fields = ['name']
+    filter_horizontal = ('users',)
 
 
 admin.site.register(Organism, OrganismAdmin)
