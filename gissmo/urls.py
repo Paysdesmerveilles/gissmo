99bd4c901d85b225619811decc1c14316621687f
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-03-21 14:27:59 +0100

    [ADD] Gissmo version is now displayed on site header

--- gissmo/urls.py
+++ gissmo/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from gissmo import __version__ as gissmo_version
+
 from intervention import views as iviews
 
-admin.site.site_header = 'The best sandbox area you ever seen!'
+admin.site.site_header = 'Gissmo %s' % gissmo_version
+admin.site.site_title = 'Gissmo'
 
 urlpatterns = [
     # Examples:
