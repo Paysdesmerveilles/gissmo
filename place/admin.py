6339618991573e30e53edbc6e3eea95667c7d05f
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-03-21 15:05:49 +0100

    [IMP] 'place' module: improve display to add built types, builts and sites

--- place/admin.py
+++ place/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
 
-from place.models import Site
+from place.models import (
+    Built,
+    BuiltType,
+    Site)
 
 
 class SiteAdmin(admin.ModelAdmin):
+    fields = ['name', '_type', 'operator']
+    list_display = ('name', '_type', 'operator')
+    list_filter = ['_type']
+    search_fields = ['name', 'operator']
+
+
+class BuiltTypeAdmin(admin.ModelAdmin):
     fields = ['name']
     list_display = ('name',)
     search_fields = ['name']
 
+
+class BuiltAdmin(admin.ModelAdmin):
+    fields = ['name', '_type', 'site']
+    list_display = ('name', '_type', 'site')
+    list_filter = ['_type']
+    search_fields = ['name', 'site']
+
 admin.site.register(Site, SiteAdmin)
+admin.site.register(BuiltType, BuiltTypeAdmin)
+admin.site.register(Built, BuiltAdmin)
