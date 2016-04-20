960300242f7b6c04313cae0a2882e5f8daaed902
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-04-20 14:58:20 +0200

    [WIP] ref #47 - Database restructuration script

---
+++ scripts/migration/main.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# WARNING: Need to use python manage.py migrate first! To deploy new 2.0 version
+
+import os
+
+HOST = os.getenv('POSTGRES_HOST', '127.0.0.1')
+DB = os.getenv('POSTGRES_DB', 'postgres')
+USER = os.getenv('POSTGRES_USER', 'postgres')
+PWD = os.getenv('POSTGRES_PASS', 'postgres')
+PORT = os.getenv('POSTGRES_PORT', '5432')
+
+import psycopg2
+
+
+def main():
+    conn_string = "host='%s' dbname='%s' user='%s' password='%s' port='%s'" % (
+        HOST,
+        DB,
+        USER,
+        PWD,
+        PORT)
+    try:
+        # START
+        conn = psycopg2.connect(conn_string)
+        print("Connected…")
+
+        # CLEAN USELESS TABLES
+        auth_message = "DROP TABLE IF EXISTS auth_message;"
+        django_site = "DROP TABLE IF EXISTS django_site;"
+        chainconfig_bck = "DROP TABLE IF EXISTS gissmo_chainconfig_bck;"
+        equip_action = "DROP TABLE IF EXISTS gissmo_equipaction;"
+        equip_state = "DROP TABLE IF EXISTS gissmo_equipstate;"
+        paramequipmodel = "DROP TABLE IF EXISTS gissmo_paramequipmodel;"
+        station_action = "DROP TABLE IF EXISTS gissmo_stationaction;"
+
+        useless_tables_sql = [
+            auth_message,
+            django_site,
+            chainconfig_bck,
+            equip_action,
+            equip_state,
+            paramequipmodel,
+            station_action]
+        for sql in useless_tables_sql:
+            cursor = conn.cursor()
+            cursor.execute(sql)
+            conn.commit()
+            cursor.close()
+
+        # CHECKS
+        # TODO: add some checks on database before
+        # TODO: builttype: create an association between old types and new ones
+
+        # SIMPLE MIGRATION
+        # TODO: add tables that are just rename or with simple migration
+        # - gissmo_equipment -> equipment_equipment
+        # - gissmo_equipsupertype -> equipment_type
+        # - gissmo_equiptype -> equipment_type (with parent)
+        # - gissmo_equipmodel -> equipment_model
+        # - gissmo_chainconfig -> equipment_configuration
+        # - gissmo_datatype -> network_datatype
+        # - gissmo_channel_data_type -> network_channel_datatypes
+        # - gissmo_equipdoc -> document_document
+        # - gissmo_equipmodeldoc -> document_document
+        # - gissmo_equipdoctype -> document_type
+        # - gissmo_equipmodeldoctype -> document_type
+        # - gissmo_forbiddenequipmentmodel -> equipment_forbiddenequipmentmodel
+        # - gissmo_intervdoc -> document_document
+        # - gissmo_intervention -> intervention_intervention
+
+        # SPECIFIC CASES
+        # TODO: add specific cases
+        # - gissmo_built -> place_place (with parent to station and site)
+        # - gissmo_chain -> network_installation (with parents)
+        # - gissmo_stationsite -> place_place
+        # - gissmo_channel -> network_channel + equipment + place
+
+        # CLEAN DATABASE
+        # TODO: delete all old gissmo_tablename tables
+        # - gissmo_channelcode
+
+        # END
+        conn.close()
+        print("Connection closed.")
+    except:
+        print("Unable to connect to the database")
+
+if __name__ == "__main__":
+    main()
