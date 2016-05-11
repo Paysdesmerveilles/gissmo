24ad54476597126f27d23bddb89a34b20bfc1c4e
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-05-11 15:15:39 +0200

    [FIX] Wrong static directory for uwsgi

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -7,13 +7,13 @@ if [ "$1" = 'development' ]; then
 # PRODUCTION ENVIRONMENT
 elif [ "$1" = 'production' ]; then
   export DJANGO_SETTINGS_MODULE=gissmo.settings.production
-  exec uwsgi --ini uwsgi.ini --pythonpath $GISSMO_DIR --static-map=/gissmo/static/=$GISSMO_UPLOAD_DIR
+  exec uwsgi --ini uwsgi.ini --pythonpath $GISSMO_DIR --static-map=/gissmo/static/=$GISSMO_STATIC_DIR
 # TEST ENVIRONMENT
 elif [ "$1" = 'test' ]; then
   export DJANGO_SETTINGS_MODULE=gissmo.settings.production
   export DEBUG=True
   export SECRET_KEY="abcdefghijklmnopqrstuvwxyz"
-  exec uwsgi --ini uwsgi.ini --pythonpath $GISSMO_DIR --static-map=/gissmo/static/=$GISSMO_UPLOAD_DIR
+  exec uwsgi --ini uwsgi.ini --pythonpath $GISSMO_DIR --static-map=/gissmo/static/=$GISSMO_STATIC_DIR
 fi
 
 exec "$@"
