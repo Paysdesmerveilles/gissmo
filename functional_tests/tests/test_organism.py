e273bc401723b436a577eaad4fafe6cc5dbfea8d
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-06-02 10:45:05 +0200

    [ADD] ref #54 - New test for Organism deletion

--- functional_tests/tests/test_organism.py
+++ functional_tests/tests/test_organism.py
@@ -38,3 +38,27 @@ class OrganismTest(FunctionalTest):
         fields = [_type, name]
 
         self.add_item_in_admin('organism/', fields, check=True)
+
+    def test_organism_deletion(self):
+        """
+        Check that an organism can be deleted
+        """
+        # Previously an organism was created
+        o = Organism.objects.create(
+            _type=Organism.UNKNOWN,
+            name='SAV')
+        # But for some reasons, it becomes useless. We so login
+        self.gissmo_login()
+        # Then delete this organism
+        url = self.appurl + 'organism/%s/delete/' % o.id
+        self.browser.get(url)
+        # Delete button
+        self.browser.find_element_by_xpath(
+            '//input[@value="Yes, I\'m sure"]').click()
+        self.browser.implicitly_wait(3)
+
+        # We check that organism is not present in the list
+        name = InputField(
+            name='name',
+            content='SAV')
+        self.check_presence_in_list('organism/', [name], presence=False)
