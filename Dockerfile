d745b7ce35afc745a4c2a6d391a02b8f34f99ea1
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-08-25 09:19:08 +0200

    [ADD] Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+FROM alpine:3.4
+
+MAINTAINER Olivier DOSSMANN, olivier+dockerfile@dossmann.net
+
+ENV GISSMO_DIR /opt/gissmo
+ENV UPLOAD_ROOT /data
+ENV STATIC_ROOT $GISSMO_DIR/static
+
+COPY . $GISSMO_DIR
+
+WORKDIR $GISSMO_DIR
+
+RUN mkdir -p $GISSMO_DIR/static $UPLOAD_ROOT
+
+RUN apk --no-cache --update add \
+        build-base \
+        libpq \
+        linux-headers \
+        mailcap \
+        postgresql-dev \
+        py-configobj \
+        python3 \
+        python3-dev && \
+    python3 -m ensurepip && \
+    pip3 install --upgrade pip && \
+    pip install --no-cache-dir --upgrade -r requirements.txt && \
+    chown -R guest:users $GISSMO_DIR $UPLOAD_ROOT && \
+    chmod -R 550 $GISSMO_DIR && \
+    chmod -R 550 $UPLOAD_ROOT && \
+    apk del \
+        build-base \
+        linux-headers \
+        postgresql-dev \
+        python3-dev && \
+    rm -rf /var/cache/apk/*
+
+VOLUME $UPLOAD_ROOT
+
+ENTRYPOINT ["./scripts/docker-entrypoint.sh"]
+
+EXPOSE 8000
+CMD ["development"]
---
+++ scripts/docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+set -e
+
+if ! [ "$POSTGRES_DB" ]; then
+  export POSTGRES_DB="postgres"
+fi
+if ! [ "$POSTGRES_HOST"] && [ "DB_PORT_5432_TCP_ADDR" ]; then
+  export POSTGRES_HOST="$DB_PORT_5432_TCP_ADDR"
+else
+  export POSTGRES_HOST="127.0.0.1"
+fi
+if ! [ "$POSTGRES_USER" ]; then
+  export POSTGRES_USER="postgres"
+fi
+if ! [ "$POSTGRES_PORT" ] && [ "DB_PORT_5432_TCP_PORT" ]; then
+  export POSTGRES_PORT="$DB_PORT_5432_TCP_PORT"
+else
+  export POSTGRES_PORT="5432"
+fi
+
+# Now having pSQL access we create static files.
+python3 manage.py collectstatic --noinput --clear -v 0
+
+# Check /data directory
+chmod -R 700 $UPLOAD_ROOT
+chown -R guest $UPLOAD_ROOT
+
+# DEVELOPEMENT ENVIRONMENT
+if [ "$1" = 'development' ]; then
+  exec python3 manage.py runserver 0.0.0.0:8000
+# PRODUCTION ENVIRONMENT
+elif [ "$1" = 'production' ]; then
+  export DJANGO_SETTINGS_MODULE=gissmo.settings.production
+  exec uwsgi --ini uwsgi.ini --pythonpath $GISSMO_DIR --static-map=/gissmo/static/=$STATIC_ROOT
+# TEST ENVIRONMENT
+elif [ "$1" = 'test' ]; then
+  export DJANGO_SETTINGS_MODULE=gissmo.settings.production
+  export DEBUG=True
+  export SECRET_KEY="abcdefghijklmnopqrstuvwxyz"
+  exec uwsgi --ini uwsgi.ini --pythonpath $GISSMO_DIR --static-map=/gissmo/static/=$STATIC_ROOT
+fi
+
+exec "$@"
